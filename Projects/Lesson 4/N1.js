b75cb3f4d97d7de9aab95ca0822c520ad637beb4
designer-developer <42907407+designer-developer@users.noreply.github.com>
2019-04-24 20:23:29 +0700

    Add files via upload

--- Projects/Lesson 4/N1.js
+++ Projects/Lesson 4/N1.js
@@ -81,20 +81,22 @@ let appData = {
                 appData.income = items.split(", ");
                 appData.income.push(prompt("Может что-то еще?", ""));
                 appData.income.sort();
-
+                appData.income.forEach(function (item, i, arr) { // Не могу понять, по какой причине алерт выскакивает ровно столько кол-во раз, сколько пользователь вводит слов                                                 
+                    alert("Способы доп. заработка: " + arr)
+                });
+            
             } else {
                 --i;
                 console.log("Ошибка, не заполненны поля");
             };
 
-        }
-        appData.income.forEach(function (item, i, arr) {                          // Не могу понять, по какой причине алерт выскакивает ровно столько кол-во раз, сколько пользователь вводит слов                                              
-            alert("Способы доп. заработка: " + arr)
-        });
+        };
+        
+        
     }
 
-}
+};
 
-for (let key in appData) {
+    for (let key in appData) {
     console.log("Наша программа включает в себя данные: " + key)
 };
