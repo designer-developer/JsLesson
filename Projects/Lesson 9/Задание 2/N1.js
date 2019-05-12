6d8deb5c8b70d5d93f6ee169ae0ce49161a49162
designer-developer <42907407+designer-developer@users.noreply.github.com>
2019-05-13 01:21:03 +0700

    Add files via upload

---
+++ Projects/Lesson 9/Задание 2/N1.js
@@ -0,0 +1,12 @@
+let age = document.getElementById('age'),
+    value = age.value;
+    
+    function showUser(surname, name) {
+        alert("Пользователь " + surname + " " + name + ", его возраст " + value);
+}
+
+showUser();
+
+
+
+
